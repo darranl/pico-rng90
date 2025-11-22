69b4ea520907dd3053b6dc1044dd16f6a8044682
Darran A Lofthouse <darran@lofthouse.me.uk>
2025-11-22 19:20:59 +0000

    Add a CRC-16 implementation to match the one used by the RNG90 IC

---
+++ crc.c
@@ -0,0 +1,69 @@
+/* Copyright 2025, Darran A Lofthouse
+ *
+ * This file is part of pico-rng90.
+ *
+ * pico-sha-test is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * pico-playground is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with pico-playground.
+ * If  not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "crc.h"
+
+uint8_t reflect(uint8_t data)
+{
+    uint8_t reflection = 0x00;
+    // Reflect the data about the center bit.
+    for (uint8_t bit = 0; bit < 8; ++bit)
+    {
+        if (data & 0x01)
+        {
+            reflection |= (1 << (7 - bit));
+        }
+        data = (data >> 1);
+    }
+    return reflection;
+}
+
+crc_t rng90_crc16(const uint8_t* data, uint8_t length)
+{
+    crc_t remainder = 0x00;
+
+    for (uint8_t pos = 0; pos < length; ++pos)
+    {
+        /*
+         * Bring the next byte into the remainder.
+         */
+        remainder ^= (reflect(data[pos]) << 8);
+
+        /*
+         * Perform modulo-2 division, a bit at a time.
+         */
+        for (uint8_t bit = 8; bit > 0; --bit)
+        {
+            /*
+             * Try to divide the current data bit.
+             */
+            if (remainder & 0x8000) // i.e. is most significant bit set
+            {
+                remainder = (remainder << 1) ^ POLYNOMIAL;
+            }
+            else
+            {
+                remainder = (remainder << 1);
+            }
+        }
+    }
+
+    /*
+     * The final remainder is the CRC result.
+     */
+    remainder = remainder ^ 0x00; // Final XOR Step
+    return remainder;
+}
---
+++ crc.h
@@ -0,0 +1,28 @@
+/* Copyright 2025, Darran A Lofthouse
+ *
+ * This file is part of pico-rng90.
+ *
+ * pico-sha-test is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * pico-playground is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with pico-playground.
+ * If  not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef RNG90_CRC_H
+#define RNG90_CRC_H
+
+#include <stdint.h>
+
+#define POLYNOMIAL 0x8005
+
+typedef uint16_t crc_t;
+
+crc_t rng90_crc16(const uint8_t* data, uint8_t length);
+
+#endif // RNG90_CRC_H
